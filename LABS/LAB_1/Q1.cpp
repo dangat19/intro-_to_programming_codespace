d2d0b00a3f180827b97ba14e63219548a1409912
XYZ <daniel.muriithi@strathmore.edu>
2026-05-14 20:56:09 +0300

    add Q1

---
+++ LABS/LAB_1/Q1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Movie\tYear\tDirector\n"
+        <<"Inception\t2010\tChristophrerNolan\n"
+        <<"The Matrix\t1999\tLana & Lilly Wachowskis\n"
+        <<"Interstellar\t2014\tChristopher Nolan\n";
+    return 0;
+}
