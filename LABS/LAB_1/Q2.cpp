9a3ae95131bc6345c125a3f53740c4329fb56833
XYZ <daniel.muriithi@strathmore.edu>
2026-05-15 06:54:09 +0300

    add Q2

---
+++ LABS/LAB_1/Q2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;   
+int main(){
+    cout<<"\"C:\\Users\\Students\\Desktop\\new_lab\\code.cpp\" ";// \\ is used to print a single backslash in the output and \" is used to print a double quote in the output.
+}
